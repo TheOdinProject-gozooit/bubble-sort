bd5997858066e9eb0f2e71df57441efa95670930
gozooit <gozoo.it@protonmail.com>
2023-04-13 12:28:49 +0200

    add loop to sort until array is sorted

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,13 +1,23 @@
 # Return a sorted array using bubble sort methodology
 def bubble_sort(arr)
-  arr.length.times do |index|
-    next if index.zero? || arr[index - 1] < arr[index]
+  until sorted?(arr)
+    arr.length.times do |index|
+      next if index.zero? || arr[index - 1] < arr[index]
 
-    tmp = arr[index]
-    arr[index] = arr[index - 1]
-    arr[index - 1] = tmp
+      tmp = arr[index]
+      arr[index] = arr[index - 1]
+      arr[index - 1] = tmp
+    end
   end
   arr
 end
 
+# Return true if the array is sorted, otherwise return false
+def sorted?(arr)
+  arr.each_with_index do |value, index|
+    next if index.zero?
 
+    return false if value < arr[index - 1]
+  end
+  true
+end
